f11a5c92f67312583fe8a3968b3f0974eee30765
c2017037 T.Sasatani <2017037@softiv.info>
2017-11-05 12:18:54 +0900

    Request取得用のAPI実装(一部出力JSONに不安要素あり)

---
+++ backend/src/main/java/jp/enpit/lama/entities/Request.java
@@ -0,0 +1,120 @@
+package jp.enpit.lama.entities;
+
+import java.util.Date;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+
+@XmlRootElement
+public class Request{
+	@XmlElement(name="id")
+	private int id;
+	@XmlElement(name="time")
+	private Date time;
+	@XmlElement(name="shop")
+	private String shop;
+	@XmlElement(name="condition")
+	private Object condition;
+	@XmlElement(name="due")
+	private Date due;
+	@XmlElement(name="clentID")
+	private int clentID;
+	@XmlElement(name="surrogateID")
+	private int surrogateID;
+	@XmlElement(name="status")
+	private String status;
+	
+	public Request(){
+		time = new Date();
+		due = new Date();
+	}
+
+	public Request(int id){
+		this();
+		setId(id);
+	}
+	
+	public Request(String shop){
+		this();
+		setShop(shop);
+	}
+
+	public Request(int id, String shop){
+		this(shop);
+		setId(id);
+	}
+
+	public Request(int id, String shop, Date time){
+		this(id, shop);
+		this.time = time;
+	}
+	
+	public Request(int id, String shop, Object condition){
+		this(id, shop);
+		setCondition(condition);
+	}
+	
+	public Request(int id, String shop, Object condition, Date due){
+		this(id, shop, condition);
+		this.due = due;
+	}
+	
+	public Request(int id, String shop, Object condition, int clentID){
+		this(id, shop, condition);
+		setClentID(clentID);
+	}
+	
+	public Request(int id, String shop, Object condition, int clentID, int surrogateID){
+		this(id, shop, condition, clentID);
+		setSurrogateID(surrogateID);
+	}
+	
+	public Request(int id, String shop, Object condition, int clentID, int surrogateID, String status){
+		this(id, shop, condition, clentID, surrogateID);
+		setStatus(status);
+	}
+
+	
+	public Date time(){
+		return time;
+	}
+	
+	public void setId(int id){
+		this.id = id;
+	}
+	
+	public int id(){
+		return id;
+	}
+	
+	public void setShop(String shop){
+		if(shop == null)
+			throw new NullPointerException();
+		this.shop = shop;
+	}
+	
+	public String shop(){
+		return shop;
+	}
+	
+	public void setCondition(Object condition){
+		this.condition = condition;
+	}
+	
+	public Object condition(){
+		return condition;
+	}
+
+	public void setClentID(int clentID){
+		this.clentID = clentID;
+	}
+	
+	public void setSurrogateID(int surrogateID){
+		this.surrogateID = surrogateID;
+	}
+	
+	public void setStatus(String status){
+		this.status = status;
+	}
+}
---
+++ backend/src/main/java/jp/enpit/lama/model/Properties.java
@@ -0,0 +1,41 @@
+package jp.enpit.lama.model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Properties {
+    public static enum Key{
+        DATEBASE_NAME,
+        HOST_NAME,
+        PORT_NUMBER;
+    };
+    private static Properties INSTANCE = new Properties();
+
+    private Map<Key, String> properties = new HashMap<>();
+
+    public static final Properties instance(){
+        return INSTANCE;
+    }
+
+    private Properties(){
+        properties.put(Key.DATEBASE_NAME, "team7");
+        properties.put(Key.HOST_NAME, "localhost");
+        properties.put(Key.PORT_NUMBER, "27017");
+    }
+
+    public String property(Key key){
+        return properties.get(key);
+    }
+
+    public String property(Key key, String defaultValue){
+        return properties.getOrDefault(key, defaultValue);
+    }
+
+    public int propertyAsInt(Key key){
+        return Integer.parseInt(property(key));
+    }
+
+    public void put(Key key, String value){
+        properties.put(key, value);
+    }
+}
